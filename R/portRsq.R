b6bce71d19cbf3c29d2ee185657b0cb3f74a2a93
Avinash Acharya <acharya@uw.edu>
2016-07-08 18:00:14 -0700

    Chnages made to R-square calculation and VIF calculation added

--- R/portRsq.R
+++ R/portRsq.R
@@ -7,9 +7,9 @@
 #' @importFrom graphics barplot
 #' 
 #' @param ffmObj   an object of class \code{ffm} produced by \code{fitFfm}
-#' @param weight   a vector of weights of the assets in the portfolio. Default is NULL.
 #' @param rsq      logical; if \code{TRUE}, R-squared values are computed for the portfolio. Default is \code{TRUE}.
 #' @param rsqAdj   logical; if \code{TRUE}, Adjusted R-squared values are computed for the portfolio. Default is \code{FALSE}.
+#' @param VIF      logical; if \code{TRUE}, Variance Inflation factor is calculated. Default is \code{FALSE}.
 #' @param digits   an integer indicating the number of decimal places to be used for rounding. Default is 2.
 #' @param ...      potentially further arguments passed.
 #' @author Avinash Acharya
@@ -35,75 +35,60 @@
 #' @export
 
 # Not the final version
-portRsqr <- function(ffmObj, weight=NULL, rsq=T, rsqAdj=F, digits=2, ...)
+portRsqr <- function(ffmObj, rsq=T, rsqAdj=F, VIF=F, digits=2, ...)
 {
   # set defaults and check input validity
   if (!inherits(ffmObj, "ffm"))
   {
     stop("Invalid argument: Object should be of class'ffm'.")
   }
-
+  
   if (!(rsq) && !(rsqAdj))
   {
     stop("Invalid arguments: Inputs rsq and rsqAdj both cannot be False.")
   }
-
-  data <- ffmObj$data
-  date.var = ffmObj$date.var
-  data <- data[order(data[,date.var]),]
-  assets.names<- ffmObj$asset.names
+  
   n.assets <- length(ffmObj$asset.names)
-  if (!is.null(weight))
-  {
-    if(length(weight) == n.assets)
-    {
-      w <- weight[assets.names]
-    }
-    else
-      stop("Error: Length of weight should be equal to the number of assets ")
-
-  }
-  else
-  {
-    w <- rep(1/n.assets, n.assets)
-  }
-
-  W<- diag(w)#NxN
-  returns = matrix(data = ffmObj$data[[ffmObj$ret.var]] , nrow = n.assets) #NxT Matrix of Returns
-  residuals = t(ffmObj$residuals) #NxT Matrix of residual returns
-  time.periods = length(ffmObj$time.periods)
-  r2 = 1 - ((t(residuals[,1:time.periods]) %*% W %*% residuals[,1:time.periods]) / (t(returns[,1:time.periods]) %*% W %*% returns[,1:time.periods]))
-  r2<- diag(r2)
-  names(r2) <- names(ffmObj$r2)
-
+  r2<- ffmObj$r2
+  
   if(rsq)
-    {
-      barplot(r2,las=2,col=5,
-              names.arg= as.yearmon(names(r2)),
-              cex.names=0.5,
-              main="R-squared Values for the Portfolio")
-      r2.mean<- round(mean(r2),digits = digits)
-      out<- r2.mean
-      names(out) <- "Mean R-Square"
-   }
+  {
+    barplot(r2,las=2,col=5,
+            names.arg= as.yearmon(names(r2)),
+            cex.names=0.5,
+            main="R-squared Values for the Portfolio")
+    r2.mean<- round(mean(r2),digits = digits)
+    names(r2.mean) <- "Mean R-Square"
+    out<- r2.mean
+  }
   if(rsqAdj)
-    {
-      K <- length(ffmObj$factor.name)
-      p <- K-1
-      adj.r2 <- 1 - ((n.assets - 1)*(1- r2) / (n.assets - p - 1))
-      barplot(adj.r2,las=2,col=5,
-          names.arg= as.yearmon(names(r2)),
-          cex.names=0.5,
-          main="Adjusted R-squared Values for the Portfolio")
-          adj.r2.mean<- round(mean(adj.r2),digits = digits)
-          out<- adj.r2.mean
-          names(out) <- "Mean Adj R-Square"
-    }
+  {
+    K <- length(ffmObj$factor.name)
+    p <- K-1
+    adj.r2 <- 1 - ((n.assets - 1)*(1- r2) / (n.assets - p - 1))
+    barplot(adj.r2,las=2,col=5,
+            names.arg= as.yearmon(names(r2)),
+            cex.names=0.5,
+            main="Adjusted R-squared Values for the Portfolio")
+    adj.r2.mean<- round(mean(adj.r2),digits = digits)
+    names(adj.r2.mean) <- "Mean Adj R-Square"
+    out<- adj.r2.mean
+  }
   if(rsqAdj && rsq)
-    {
-      out<- rbind(r2.mean, adj.r2.mean)
-      #row.names(out)<- c("Mean R-Square", "Mean Adj R-Square")
-    }
+  {
+    out<- c(r2.mean, adj.r2.mean)
+  }
+  if(VIF)
+  {
+    object = ffmObj$factor.returns  
+    object <- as.matrix(object)
+    ncols <- dim(object)[2]
+    vif = lapply(seq(ncols), function(x) 1/(1 - summary(lm(object[, x] ~ 
+                                                             object[, -x]))$r.squared))
+    names(vif) <- dimnames(object)[[2]]
+    vif = unlist(vif)
+    out<- c(out, list("VIF" = vif))
+    
+  }
   print(out)
 }
-
--- tests/testthat/test-portRsqr.R
+++ tests/testthat/test-portRsqr.R
@@ -1,7 +1,6 @@
 #Load the data 
 data("factorDataSetDjia5Yrs")
-data("wtsDjiaGmvLo")
-w = wtsDjiaGmvLo
+
 #Fit a Ffm
 fit <- fitFfm(data=factorDataSetDjia5Yrs, asset.var="TICKER", ret.var="RETURN", 
               date.var="DATE", exposure.vars="SECTOR")
@@ -19,7 +18,4 @@ expect_equal(length(out), 2)
 #test for error msg
 expect_error(portRsqr(fit, rsq = F, rsqAdj = F), 
              "Invalid arguments: Inputs rsq and rsqAdj both cannot be False") 
-#Make w=N+1 to generate error msg
-w<- c(w,"dummy" = 0.02)
-expect_error(portRsqr(fit, rsq = T, rsqAdj = F, weight = w),
-             "Error: Length of weight should be equal to the number of assets") 
+
