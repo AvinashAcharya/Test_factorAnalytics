953b20e3646b539bd3c16c51a7f3a4fd7e3e7bd8
chrisyi1991 <yilingjie1991@gmail.com>
2016-07-09 15:37:25 -0700

    edit testing files

--- tests/testthat/test-portSdDecomp.R
+++ tests/testthat/test-portSdDecomp.R
@@ -18,7 +18,7 @@ fit <- fitFfm(data = dat,
 
 #generating statistic
 expect_equal(is.list(portSdDecomp(fit, wtsStocks145GmvLo, isPlot = FALSE)), TRUE) 
-expect_equal(is.list(portSdDecomp(fit, wtsStocks145GmvLo, isPlot = TRUE)), TRUE) 
+expect_equal(is.list(portSdDecomp(fit, wtsStocks145GmvLo, isPlot = TRUE, which = 1)), TRUE) 
 
 #testing error message
 expect_error(portSdDecomp(fit, weights = c(0.5,0.5), isPlot = TRUE, which = 1,
